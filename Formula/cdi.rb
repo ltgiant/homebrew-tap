1c6135ee47eb5c94b8a321392078f101f0bea775
ltgiant <jihoonseo272@gmail.com>
2026-03-11 12:18:47 +0900

    Update cdi formula with sha256 checksums

--- Formula/cdi.rb
+++ Formula/cdi.rb
@@ -7,16 +7,16 @@ class Cdi < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ltgiant/cdi/releases/download/v#{version}/cdi-aarch64-apple-darwin.tar.gz"
-      # sha256 will be updated after first release
+      sha256 "ec4873098edbafd38d8e175ea5f43c2f3261e6983de4be6eb88073ded1108206"
     else
       url "https://github.com/ltgiant/cdi/releases/download/v#{version}/cdi-x86_64-apple-darwin.tar.gz"
-      # sha256 will be updated after first release
+      sha256 "ec1c3519a620b243f4a28bfbbc8488b06bc33da3076635b8d2f8785180cfdef5"
     end
   end
 
   on_linux do
     url "https://github.com/ltgiant/cdi/releases/download/v#{version}/cdi-x86_64-unknown-linux-gnu.tar.gz"
-    # sha256 will be updated after first release
+    sha256 "1c92080eb6611062daa723a91372a062b1f1b00170d74156072377ce438d2826"
   end
 
   def install
